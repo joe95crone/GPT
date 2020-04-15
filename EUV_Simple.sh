48c7cb1e285c5e8926e014f17db13f96549b21e4
Joe Crone <63721714+joe95crone@users.noreply.github.com>
2020-04-15 18:53:54 +0100

    Add files via upload

---
+++ EUV_Simple.sh
@@ -0,0 +1,2 @@
+/opt/GPT3.3.6/bin/gpt -v -o /home/fgg55738/GPT_Simple/EUV_Simple.gdf /home/fgg55738/GPT_Simple/EUV_Simple.in GPTLICENSE=1299668842
+/opt/GPT3.3.6/bin/gdf2a -o /home/fgg55738/GPT_Simple/EUV_Simple.txt /home/fgg55738/GPT_Simple/EUV_Simple.gdf
---
+++ EUV_microbunch.sh
@@ -0,0 +1,2 @@
+/opt/GPT3.3.6/bin/gpt -v -o /home/fgg55738/GPT_microbunching/EUV_raw.gdf /home/fgg55738/GPT_microbunching/EUV_microbunch.in GPTLICENSE=1299668842
+/opt/GPT3.3.6/bin/gdf2a -o /home/fgg55738/GPT_microbunching/EUV_raw.txt /home/fgg55738/GPT_microbunching/EUV_raw.gdf
